844d6f95f9134ee9459a086a668ff8281b65c9d7
martisgit <martisgit@github.com>
2024-09-22 20:39:31 +0300

    recipesApp

---
+++ script.js
@@ -0,0 +1,42 @@
+const form = document.getElementById("recipe-form");
+const successMessage = document.getElementById("success-message");
+
+form.addEventListener("submit", (submitForm) => {
+  submitForm.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const description = document.getElementById("description").value;
+  const instructions = document.getElementById("instructions").value;
+  const ingredients = document.getElementById("ingredients").value;
+  const recipe_img = document.getElementById("recipe_img").value;
+
+  const recipe = {
+    title: title,
+    description: description,
+    instructions: instructions,
+    ingredients: ingredients,
+    recipe_img: recipe_img,
+  };
+
+  fetch("https://66f03a03f2a8bce81be55c6c.mockapi.io/recipes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(recipe),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+  successMessage.style.display = "block";
+
+  setTimeout(() => {
+    window.location.href = "./recipes/index.html";
+  }, 3000);
+  form.reset();
+});
